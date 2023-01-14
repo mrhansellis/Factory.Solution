59df5edffbf0a8eb6aba3560d7a2953e219ec3f2
Hans Ellis <bullhorn@gmail.com>
2023-01-14 13:24:57 -0800

    add machine deletejoin controller

--- Factory/Controllers/MachineController.cs
+++ Factory/Controllers/MachineController.cs
@@ -94,5 +94,14 @@ namespace Factory.Controllers
       }
       return RedirectToAction("Details", new { id = machine.MachineId });
     }
+
+    [HttpPost]
+    public ActionResult DeleteJoin(int joinId)
+    {
+      EngineerMachine joinEntry = _db.EngineerMachines.FirstOrDefault(entry => entry.EngineerMachineId == joinId);
+      _db.EngineerMachines.Remove(joinEntry);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
